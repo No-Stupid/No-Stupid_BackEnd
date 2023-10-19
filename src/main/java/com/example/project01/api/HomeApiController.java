57e894e2191eb6aa43e06c309ad1b459b756d536
yi-jin <dlwls4338@naver.com>
2023-10-19 23:52:00 +0900

    initial commit

---
+++ src/main/java/com/example/project01/api/HomeApiController.java
@@ -0,0 +1,14 @@
+package com.example.project01.api;
+
+import com.example.project01.argumentResolver.Login;
+import com.example.project01.entity.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class HomeApiController {
+
+}
